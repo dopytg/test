e4659c88376c62a22ff7be9a893131ff4df85027
tim <esr@gmail.com>
2019-06-17 15:42:32 +0600

    add js code

--- object/script.js
+++ object/script.js
@@ -1,1 +1,24 @@
-console.log("hh");
+
+ let money = prompt ( "Ваш бюджет на месяц? " , "");
+
+ let time = prompt ( "Введите дату в формате YYY-MM-DD " , "");
+
+let appData = {
+    budjet: money,
+    timeData: time,
+    expences:{
+        "options":"howMach"
+    },
+    optionalExpences: "",
+    income: "",
+    savings: false
+};
+
+var options = prompt (" Введите обязательную статью расходов в этом месяце ",""),
+    howMach = prompt ( "Во сколько обойдется? ","");
+
+    alert ("Бюджет на один день = " );
+
+
+    
+
